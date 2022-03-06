58f2f1b4317e9cc5aba9d8d2cce0e9cf8661f52e
宋瑶 <songyao@qutoutiao.net>
2022-03-07 00:06:03 +0800

    feat: css预处理器

--- vue/myLabViteVue/src/App.vue
+++ vue/myLabViteVue/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" title="vite vue log" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
