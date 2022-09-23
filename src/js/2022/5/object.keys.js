ab82454c9ba3d2ff885f1b327c71d4d89d3ee064
宋瑶 <songyao@yuewen.com>
2022-09-23 10:19:33 +0800

    feat: update 220923001

--- src/js/2022/5/object.keys.js
+++ src/js/2022/5/object.keys.js
@@ -8,3 +8,17 @@ const obj2 = {
 console.log(Object.keys(obj2))
 
 console.log('number', Object.keys('123'))
+const obj = {}
+obj[-1] = -1
+obj[1] = 1
+obj[1.1] = 1.1
+obj['2'] = '2'
+obj['c'] = 'c'
+obj['b'] = 'b'
+obj['a'] = 'a'
+obj[2] = 2
+obj[Symbol(1)] = Symbol(1)
+obj[Symbol('a')] = Symbol('a')
+obj[Symbol('b')] = Symbol('b')
+obj['d'] = 'd'
+console.log(Object.keys(obj))
