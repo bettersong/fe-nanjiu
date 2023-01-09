06b5371ed11b3ab4ea04f4d78a96ec46cc7614c7
songyao <songyao0606@163.com>
2023-01-09 16:56:05 +0800

    feat: babel

---
+++ 工程化/babel/src/babelLearn2/compile.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+const babel = require('@babel/core')
+
---
+++ 工程化/babel/src/babelLearn2/src/index.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '前端南玖'
+        }
+    },
+    methods: {
+        fn() {
+            console.log('fn')
+        }
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
